042c9fcc95e80392f81c96fa68276d0b31821596
AEOB <eloiisaorozco@gm>
2025-11-04 20:39:56 +0000

    Updated to take the model from the envs

--- slide_content_agent/agent.py
+++ slide_content_agent/agent.py
@@ -1,3 +1,4 @@
+import os
 from google.adk.agents import LlmAgent
 from typing import List
 
@@ -14,17 +15,17 @@ illustration_agent = RemoteA2aAgent(
 )
 
 root_agent = LlmAgent(
-            model='gemini-2.0-flash-001',
-            name='slide_content_agent',
-            description='An agent that writes content for slide decks.',
-            #after_tool_callback=self._handle_auth_required_task,
-            instruction="""
-                A user will ask you to create content for a slide to communicate an idea.
-                Write a short headline about this idea.
-                Write 1-2 sentences of body text about this idea.
-                Share these with the user.
-                Then transfer to the 'illustration_agent' to generate an illustration related to this idea.
-                """,
-	        # sub_agents parameter
-            sub_agents=[illustration_agent]
-        )
+    model=os.getenv("MODEL"),
+    name='slide_content_agent',
+    description='An agent that writes content for slide decks.',
+    #after_tool_callback=self._handle_auth_required_task,
+    instruction="""
+        A user will ask you to create content for a slide to communicate an idea.
+        Write a short headline about this idea.
+        Write 1-2 sentences of body text about this idea.
+        Share these with the user.
+        Then transfer to the 'illustration_agent' to generate an illustration related to this idea.
+        """,
+    # sub_agents parameter
+    sub_agents=[illustration_agent]
+)
