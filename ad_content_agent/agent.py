ff3f61a8f601a7dc16818a1044a22b28036e4f80
AEOB <eo@gm>
2025-11-27 03:42:07 +0000

    Music class ad generation agent - Moved illustration_agent to be a remote A2A Agent, updated agent card

--- ad_content_agent/agent.py
+++ ad_content_agent/agent.py
@@ -2,23 +2,25 @@ import os
 from google.adk.agents import LlmAgent
 from typing import List
 
+from google.adk.agents.remote_a2a_agent import AGENT_CARD_WELL_KNOWN_PATH
 from google.adk.agents.remote_a2a_agent import RemoteA2aAgent
 
+
+print("AGENT_CARD_WELL_KNOWN_PATH:", AGENT_CARD_WELL_KNOWN_PATH)    # /.well-known/agent-card.json
 
 # Agents
 illustration_agent = RemoteA2aAgent(
     name="illustration_agent",
     description="Agent that generates illustrations.",
-    agent_card=(
-        "illustration-agent-card.json"
-    ),
+    #agent_card=f"http://localhost:8001{AGENT_CARD_WELL_KNOWN_PATH}",
+    #agent_card=f"https://illustration-agent-485230244166.us-central1.run.app/a2a/illustration_agent{AGENT_CARD_WELL_KNOWN_PATH}",
+    agent_card="https://illustration-agent-485230244166.us-central1.run.app/a2a/illustration_agent/.well-known/agent-card.json",
 )
 
 root_agent = LlmAgent(
     model=os.getenv("MODEL"),
     name='ad_content_agent',
     description='An agent that writes content for advertisement.',
-    #after_tool_callback=self._handle_auth_required_task,
     instruction="""
         A user will ask you to create content for an ad to communicate an idea.
         Write a short headline about this idea.
@@ -28,3 +30,10 @@ root_agent = LlmAgent(
         """,
     sub_agents=[illustration_agent]
 )
+
+
+# For debugging - print confirmation when module loads
+print(f"\n🎼 🎶 🎵 Music class ad generation - Ad Content agent")
+print(f"\tName: {root_agent.name}")
+print(f"\tModel: {root_agent.model}")
+print(f"\tInstructions length: {len(root_agent.instruction)} characters")
--- remote_a2a/illustration_agent/agent.py
+++ remote_a2a/illustration_agent/agent.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 
 # ADK imports
 from google.adk import Agent
+from google.adk.a2a.utils.agent_to_a2a import to_a2a
 from google.adk.agents import SequentialAgent, LoopAgent, ParallelAgent
 from google.adk.tools.tool_context import ToolContext
 from .agent_instructions import ILLUSTRATOR_DESCRIPTION, ILLUSTRATOR_INSTRUCTIONS
@@ -21,8 +22,17 @@ load_dotenv()
 # SDK Initialization
 project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
 location = os.getenv("GOOGLE_CLOUD_LOCATION", "global")
-client = genai.Client(vertexai=True, project=project_id, location=location)
+google_genai_use_vertexai = os.getenv("GOOGLE_GENAI_USE_VERTEXAI")
+print("\nproject_id:", project_id)
+print("location:", location)
+print("google_genai_use_vertexai:", google_genai_use_vertexai)
 
+IMAGE_MODEL = os.getenv("IMAGE_MODEL")
+print("IMAGE_MODEL:", IMAGE_MODEL, "\n")
+
+
+# Provide the required arguments either directly or by using environment variables
+client = genai.Client()
 
 # Tool
 def generate_image(prompt: str) -> dict[str, str]:
@@ -36,7 +46,7 @@ def generate_image(prompt: str) -> dict[str, str]:
     """
     # 1. Call the model
     response = client.models.generate_content(
-        model=os.getenv("IMAGE_MODEL"),
+        model=IMAGE_MODEL,
         contents=prompt,
         config=GenerateContentConfig(
             response_modalities=["IMAGE"],
@@ -77,3 +87,13 @@ root_agent = Agent(
     instruction=ILLUSTRATOR_INSTRUCTIONS,
     tools=[generate_image]
 )
+
+a2a_app = to_a2a(root_agent)
+
+
+# For debugging - print confirmation when module loads
+print(f"\n🎼 🎶 🎵 Music class ad generation - Illustration agent")
+print(f"\tName: {root_agent.name}")
+print(f"\tModel: {root_agent.model}")
+print(f"\tInstructions length: {len(root_agent.instruction)} characters")
+print(f"\tTools: {len(root_agent.tools)} configured\n")
